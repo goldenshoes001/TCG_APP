f17dbd347b54fe4098e0397399524d574695ff73
goldenshoes001 <sebim1993@googlemail.com>
2025-07-21 16:15:55 +0200

    app bar verbessert

--- lib/class/appbar.dart
+++ lib/class/appbar.dart
@@ -1,25 +1,47 @@
 import 'package:flutter/material.dart';
-import 'package:tcg_app/class/appdata.dart';
 
 class Barwidget extends StatelessWidget implements PreferredSizeWidget {
+  final MainAxisAlignment titleFlow;
   final Color barColor;
   final String title;
+  final double fontSize;
+  final Color textColor;
+  final FontWeight fontWeight;
   final int elevation;
   final Color shadow;
   final Color surfaceTintColor;
+  final double height;
   const Barwidget({
     super.key,
+    this.titleFlow = MainAxisAlignment.center,
     this.barColor = Colors.black,
-    this.title = "",
+    this.title = "Platzhalter",
+    this.fontSize = 24,
+    this.textColor = const Color(0xFF456585),
+    this.fontWeight = FontWeight.normal,
     this.elevation = 0,
     this.shadow = Colors.transparent,
     this.surfaceTintColor = Colors.transparent,
+    this.height = 40,
   });
   @override
   Widget build(BuildContext context) {
     return AppBar(
       backgroundColor: barColor,
-      title: Text(title),
+      centerTitle: false,
+      title: Row(
+        mainAxisAlignment: titleFlow,
+        children: [
+          Text(
+            title,
+            style: TextStyle(
+              fontSize: fontSize,
+              color: textColor,
+              fontWeight: fontWeight,
+            ),
+          ),
+        ],
+      ),
       elevation: 0,
       shadowColor: shadow,
       surfaceTintColor: surfaceTintColor,
@@ -27,5 +49,5 @@ class Barwidget extends StatelessWidget implements PreferredSizeWidget {
   }
 
   @override
-  Size get preferredSize => Size.fromHeight(Appdata.appbarSize);
+  Size get preferredSize => Size.fromHeight(height);
 }
